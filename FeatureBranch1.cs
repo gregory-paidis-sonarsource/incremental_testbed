56a02fdfa1e10eae476ba2cbef1a19f01ca226ca
Gregory Paidis <gregory.paidis@sonarsource.com>
2023-02-24 13:08:08 +0100

    Initial commit

---
+++ FeatureBranch1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace incremental_testbed
+{
+    internal class FeatureBranch1
+    {
+        private int MeaningOfLife = 42;
+
+        public void DoWork()
+        {
+            Console.WriteLine("Hello from feature branch #1");
+        }
+
+        private bool UselessInFirstPR = true;
+        void UselessMethodInFirstPr()
+        {
+
+        }
+
+        //
+    }
+}
---
+++ FeatureBranch2.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace incremental_testbed
+{
+    public class FeatureBranch2
+    {
+        public void DoWork()
+        {
+            Console.WriteLine("Hello from feature branch #2");
+        }
+
+        void Empty()
+        {
+
+        }
+
+        void UselessMethodInFirstPr()
+        {
+
+        }
+    }
+}
